5df0a737db1a6b42b26c9480a6d54d7902c96969
Saman <saman.ashoori@outlook.com>
2026-02-10 00:08:34 +0000

    Refactor import statement for Svelte plugin in vite.config.js for consistency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import svelte from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   server: {
     proxy: {
-      // "If the frontend asks for /api, forward it to port 8000"
       '/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
